7826df08da2f8f6465d349b1b95086628068364b
khalifanayna <khalifa24ti@mahasiswa.pcr.ac.id>
2026-01-07 18:36:17 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
+
+
 
 
