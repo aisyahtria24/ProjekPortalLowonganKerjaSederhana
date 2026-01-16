4e19a01f5c7f28e7c7a7ed5b0c4ee5eb4f04fcf4
khalifanayna <khalifa24ti@mahasiswa.pcr.ac.id>
2026-01-16 17:25:33 +0700

    5

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,64 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lamaran;
+use App\Models\Lowongan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-{
-        return view('admin.dashboard');
-
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+  public function index()
+  {
+    $data['title'] = 'Dashboard';
+    $user = Auth::user();
+    $data['lowongans_count'] = Lowongan::when($user->role != "admin", function ($query) {
+      $query->where('tgl_buka', '>=', date('Y-m-d'));
+    })->count();
+    $data['lamarans'] = Lamaran::select('id', 'status')->get();
+    $data['lamarans_count'] = $data['lamarans']->count();
+    $data['diterima'] = $data['lamarans_count'] > 0 ? ceil(($data['lamarans']->where('status', 'Diterima')->count() / $data['lamarans_count']) * 100) : 0;
+    return view('dashboard', $data);
+  }
 }
