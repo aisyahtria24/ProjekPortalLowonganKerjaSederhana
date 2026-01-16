4c2871312254f7e7f841222562b3369a95a6225d
khalifanayna <khalifa24ti@mahasiswa.pcr.ac.id>
2026-01-17 03:10:55 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Lamaran;
+use App\Models\Lowongan;
+use App\Models\LowonganFiles;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+         \App\Models\User::factory()->create();
+         Lowongan::factory()->create();
+         Lamaran::factory()->create();
+         LowonganFiles::factory()->create();
     }
 }
