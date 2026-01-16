f7e1a88d8f28a49aed2237a560c736dad5cd4af3
aisyahtria24 <tria24ti@mahasiswa.pcr.ac.id>
2026-01-16 17:34:18 +0700

    6

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Middleware\AdminMiddleware;
@@ -7,5 +8,5 @@ use App\Http\Middleware\AdminMiddleware;
 Route::get('/dashboard', [DashboardController::class, 'index']);
 
 
-
+Route::get('/', [HomeController::class, 'index'])->middleware('guest')->name('home');
 
